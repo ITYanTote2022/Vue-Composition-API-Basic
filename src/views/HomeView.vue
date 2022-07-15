70e9f77bc8ee71a8260486724ce5e90238a42f2b
IT YanTote <moemyintzaw48@gmail.com>
2022-07-15 22:30:09 +0630

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div>
+      <p>My name is {{name}}.I am {{age}} years old.</p>
+    <button @click="changeName">Change Name</button>
+    <button @click="increaseAge">Increase Age</button>
+    </div>
+    <br>
+    <hr>
+    <br>
+
+    <input type="text" v-model="search">
+    <p>Search items - {{search}}</p>
+    <div v-for="person in filterPersons" :key="person">
+            {{person}}
+    </div>
+
+     
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core';
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  setup(){
+    let name = ref("Kyaw Kyaw");
+    let age = ref(20);
+
+    let changeName=()=>{
+      name.value="Mg Mg"
+    };
+
+    let increaseAge=()=>{
+      age.value++
+    };
+
+
+    let persons=ref(['Zaw Zaw','Mg Mg','Su Su','Tun Tun']);
+
+    let search = ref("");
+
+    let filterPersons=computed(()=>{
+      return persons.value.filter((person)=>{
+        return person.includes(search.value)
+      })
+    })
+
+    return{name,age,changeName,increaseAge,persons,search,filterPersons};
   }
 }
 </script>
